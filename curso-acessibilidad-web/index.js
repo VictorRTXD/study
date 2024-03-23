3a812bb6f1dafd75aae587f9f449265acca79cec
Victor <89050826+VictorRTXD@users.noreply.github.com>
2024-03-23 17:09:37 +0000

    empezando clase 30

--- curso-acessibilidad-web/index.js
+++ curso-acessibilidad-web/index.js
@@ -21,6 +21,25 @@ function clickRight() {
   }
   let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
   document.querySelector(".project-container").style.left = `${newValue}px`;
+
+  switch (newValue) {
+    case -270:
+      document.querySelector(".project1").setAttribute("tabindex", "-1");
+      document.querySelector(".project1-container").setAttribute("aria-hidden", true);
+      document.querySelector(".project4").removeAttribute("tabindex");
+      document.querySelector(".project4-container").removeAttribute("aria-hidden");
+      break;
+
+    case -540:
+      document.querySelector(".project2").setAttribute("tabindex", "-1");
+      document.querySelector(".project2-container").setAttribute("aria-hidden", "true");
+      document.querySelector(".project5").removeAttribute("tabindex");
+      document.querySelector(".project5-container").removeAttribute("aria-hidden");
+      break;
+  
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
@@ -34,6 +53,25 @@ function clickLeft() {
   }
   let newValue = currentLeft + 270;
   document.querySelector(".project-container").style.left = `${newValue}px`;
+
+  switch (newValue) {
+    case -270:
+      document.querySelector(".project5").setAttribute("tabindex", "-1");
+      document.querySelector(".project5-container").setAttribute("aria-hidden", true);
+      document.querySelector(".project2").removeAttribute("tabindex");
+      document.querySelector(".project2-container").removeAttribute("aria-hidden");
+      break;
+
+    case -540:
+      document.querySelector(".project4").setAttribute("tabindex", "-1");
+      document.querySelector(".project4-container").setAttribute("aria-hidden", "true");
+      document.querySelector(".project1").removeAttribute("tabindex");
+      document.querySelector(".project1-container").removeAttribute("aria-hidden");
+      break;
+  
+    default:
+      break;
+  }
 }
 
 /** Esta funcion se llama cuando la persona hace click en el boton de enviar del formulario de contacto */
