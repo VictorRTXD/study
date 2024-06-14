c32c695ab7ecd7a6ba7f741c5c675ce0a732fb08
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-06-14 08:56:41 +0000

    Usando react router doom clase 4

--- curso-react-vite-tailwind/react-course/src/Pages/App/index.jsx
+++ curso-react-vite-tailwind/react-course/src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
@@ -6,15 +7,26 @@ import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 
+function AppRoutes() {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sign-in', element: <SignIn /> },
+  ])
+
+  return routes;
+}
+
 function App() {
+
   return (
     <div className='bg-green-500'>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </div>
   )
 }
