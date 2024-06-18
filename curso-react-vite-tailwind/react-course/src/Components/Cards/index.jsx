0713184e930f38016d8ec74f3f75f6fb3eb4fb61
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-06-18 00:17:53 +0000

    acabando clase 7 y 8, anadiendo componente card y usando api para hacer cards

--- curso-react-vite-tailwind/react-course/src/Components/Cards/index.jsx
+++ curso-react-vite-tailwind/react-course/src/Components/Cards/index.jsx
@@ -1,18 +1,19 @@
-function Cards() {
+function Cards({ product }) {
+    const {category, images, title, price } = product 
     return (
         <div className=" bg-white cursor-pointer w-56 h-60 rounded-lg">
             <figure className=" relative mb-2 w-full h-4/5">
                 <span className=" absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-                    Electronics
+                    {category.name}
                 </span>
-                <img className="w-full h-full object-cover rounded-lg" src="https://tienda.redcomputer.es/55620-thickbox_default/auriculares-asus-rog-cetra-true-wireless.jpg" alt="headphones image" />
+                <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt={`imagen de ${title}`} />
                 <div className=" absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
                     +
                 </div>
             </figure>
             <p className="flex justify-between bottom-0">
-                <span className="text-sm font-light">Headphones</span>
-                <span className="text-lg font-medium">$300</span>
+                <span className="text-sm font-light">{title}</span>
+                <span className="text-lg font-medium">{`$${price}`}</span>
             </p>
         </div>
     );
--- curso-react-vite-tailwind/react-course/src/Pages/Home/index.jsx
+++ curso-react-vite-tailwind/react-course/src/Pages/Home/index.jsx
@@ -1,11 +1,27 @@
+import React from "react";
+import { useState, useEffect } from "react";
 import { Layout } from "../../Components/Layout"
 import { Cards } from "../../Components/Cards"
 
 function Home() {
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItem(data))
+  }, [])
+
   return (
     <Layout>
       Home
-      <Cards />
+      <div className="grid w-full grid-cols-4 gap-4 max-w-screen-lg ">
+      {
+        item?.map((item) => {
+          return <Cards product={item} key={item.id}  />
+        })
+      }
+      </div>
     </Layout>
   )
 }
