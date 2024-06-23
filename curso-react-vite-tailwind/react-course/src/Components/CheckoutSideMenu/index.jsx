392dde524a65cdf59c9217a817ec3cb4be185248
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-06-23 23:39:49 +0000

    acabando clase 17, creando OrderCard

--- curso-react-vite-tailwind/react-course/src/Components/CheckoutSideMenu/index.jsx
+++ curso-react-vite-tailwind/react-course/src/Components/CheckoutSideMenu/index.jsx
@@ -2,15 +2,22 @@ import "./styles.css"
 import { useContext } from "react";
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from "../../Context";
+import { OrderCard } from "../OrderCard";
 
 function CheckoutSideMenu() {
-    const {isOpenCheckoutSideMenu, closeCheckoutSideMenu} = useContext(ShoppingCartContext);
+    const {isOpenCheckoutSideMenu, closeCheckoutSideMenu, cartProducts} = useContext(ShoppingCartContext);
+    console.log(cartProducts);
     
     return (
         <aside className={`${isOpenCheckoutSideMenu ? "flex" : "hidden"} checkout-side-menu  flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className="flex justify-between items-center p-6">
                 <h2 className=" font-medium text-xl">My order</h2>
                 <XMarkIcon onClick={() => closeCheckoutSideMenu()} className="size-6 text-black-500 cursor-pointer" />
+            </div >
+            <div className="px-6">
+                {cartProducts.map(product => (
+                    <OrderCard title={product.title} imageUrl={product.images} price={product.price} key={product.id}/>
+                ))}
             </div>
         </aside>
     );
