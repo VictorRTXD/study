4da88bfdf187411dd0b76bf6a3f8ada9d9fd6b81
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2024-12-21 18:38:23 -0600

    finalizando clase 11 angular 17, (aprendiendo a actualizar y eliminar elementos en array singals)

--- todoapp/src/app/pages/home/home.component.ts
+++ todoapp/src/app/pages/home/home.component.ts
@@ -9,4 +9,17 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
     tasks2 = signal(["instalar angular", "crear proyecto", "crear componentes"]);
+
+    changeHandler(event: Event) {
+      // const entrada = document.querySelector(".new-todo");
+      // entrada?.setAttribute("value", "");
+      const input = event.target as HTMLInputElement;
+      const newTasks = input.value;
+      // a diferencia del set que reinicia el valor, update lo actualiza
+      this.tasks2.update((tasks2) => [...tasks2, newTasks]);
+    }
+
+    deleteTask(index: number) {
+      this.tasks2.update((tasks2) => tasks2.filter((task, position) => position != index));
+    }
 }
