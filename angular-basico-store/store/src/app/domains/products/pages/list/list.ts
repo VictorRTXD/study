cd7cac93d14308052decd434faca278a60bd9324
Victor <89050826+VictorRTXD@users.noreply.github.com>
2025-09-11 00:36:38 +0000

    acabando clase 43 angular basico

--- angular-basico-store/store/src/app/domains/products/pages/list/list.ts
+++ angular-basico-store/store/src/app/domains/products/pages/list/list.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../components/product/product';
 import { ProductModel } from '../../../shared/componentes/counter/models/product.model';
+import { Header } from '../../../shared/components/header/header';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, Product],
+  imports: [CommonModule, Product, Header],
   templateUrl: './list.html',
   styleUrl: './list.css'
 })
