f7370082c43edf76d05881a0dc8eeb9f912a52f3
Victor <89050826+VictorRTXD@users.noreply.github.com>
2025-09-12 01:53:57 +0000

    acabando clase 46 amgular basico

--- angular-basico-store/store/src/app/domains/shared/components/header/header.ts
+++ angular-basico-store/store/src/app/domains/shared/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, signal, SimpleChange, SimpleChanges } from '@angular/core';
 import { ProductModel } from '../../componentes/counter/models/product.model';
 import { CommonModule } from '@angular/common';
 
@@ -11,8 +11,20 @@ import { CommonModule } from '@angular/common';
 export class Header {
   hideSideMenu = signal(true);
   @Input({required: true}) cart: ProductModel[] = []
+  total = signal(0);
 
   toogleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if (cart) {
+      this.total.set(this.calcTotal())
+    }
+  }
+
+  calcTotal() {
+    return this.cart.reduce((total, product) => total + product.price, 0)
   }
 }
