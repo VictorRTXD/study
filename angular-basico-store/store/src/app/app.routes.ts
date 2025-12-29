e604b9b2b5c219cdad91869f11ec77343f76ca65
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2025-12-29 23:53:32 +0000

    acabando clase 61 angular basico

--- angular-basico-store/store/src/app/app.routes.ts
+++ angular-basico-store/store/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { About } from './domains/info/pages/about/about';
 import { List } from './domains/products/pages/list/list';
 import { NotFound } from './domains/info/pages/not-found/not-found';
 import { Layout } from '@shared/components/layout/layout';
+import { ProductDetail } from '@products/pages/product-detail/product-detail';
+ProductDetail
 
 export const routes: Routes = [
     {
@@ -16,6 +18,10 @@ export const routes: Routes = [
             {
                 path: 'about',
                 component: About
+            },
+            {
+                path: 'product/:id',
+                component: ProductDetail
             },
         ]
     },
---
+++ angular-basico-store/store/src/app/domains/products/pages/product-detail/product-detail.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [CommonModule],
+  templateUrl: './product-detail.html',
+  styleUrl: './product-detail.css',
+  standalone: true,
+})
+export class ProductDetail {
+
+}
