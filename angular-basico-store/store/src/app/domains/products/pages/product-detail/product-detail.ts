7b1937382bd0e76d66d701988e4050355e822fba
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2026-01-07 07:05:20 +0000

    acabando clase 65 angular basico

--- angular-basico-store/store/src/app/domains/products/pages/product-detail/product-detail.ts
+++ angular-basico-store/store/src/app/domains/products/pages/product-detail/product-detail.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, input, signal } from '@angular/core';
-import { Product } from '@products/components/product/product';
 import { ProductModel } from '@shared/componentes/counter/models/product.model';
 import { ProductService } from '@shared/services/product.service';
+import { Cart } from '@shared/services/cart';
 
 @Component({
   selector: 'app-product-detail',
@@ -14,16 +14,33 @@ import { ProductService } from '@shared/services/product.service';
 export class ProductDetail {
   @Input() id?: string;
   product = signal<ProductModel | null>(null);
+  cover = signal("")
+
   private productService = inject(ProductService);
+  private cartService = inject(Cart)
   
   ngOnInit() {
     if (this.id) {
       this.productService.getProduct(this.id)
       .subscribe({
         next: (product) => {
-          this.product.set(product)
+          this.product.set(product);
+          if (product.images.length > 0 ) {
+            this.cover.set(product.images[0])
+          }
         }
       })
+    }
+  }
+
+  changeCover(newImage: string) {
+    this.cover.set(newImage)
+  }
+
+  addToCart() {
+    const product = this.product();
+    if (product) {
+      this.cartService.addToCart(product)
     }
   }
 }
