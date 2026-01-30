9afc542847729f7eda61b3e60150259db44a1350
VictorRTXD <maciasvictoreduardo5736@gmail.com>
2026-01-29 18:53:09 -0600

    acabando clase 65 angular

--- angular-basico-store/store/src/app/domains/products/pages/list/list.ts
+++ angular-basico-store/store/src/app/domains/products/pages/list/list.ts
@@ -2,23 +2,35 @@ import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../components/product/product';
 import { ProductModel } from '@shared/componentes/counter/models/product.model';
-import { Header } from '@shared/components/header/header';
 import { Cart } from '@shared/services/cart';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category';
+import { Category } from '@shared/componentes/counter/models/category.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, Product, Header],
+  imports: [CommonModule, Product, ],
   templateUrl: './list.html',
   styleUrl: './list.css'
 })
 export class List {
   products = signal<ProductModel[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(Cart)
+  private categoryService = inject(CategoryService)
   private productService = inject(ProductService);
 
   ngOnInit() {
+    this.getProducts()
+    this.getCategories()
+  }
+
+  addToCart(product: ProductModel) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
     this.productService.getProducts()
     .subscribe({
       next: (products) => {
@@ -30,7 +42,15 @@ export class List {
     })
   }
 
-  addToCart(product: ProductModel) {
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getAll()
+    .subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: () => {
+        
+      }
+    })
   }
 }
