af3570bcaee183b1978f7762e38d63a9b02fdd45
VictorRTXD <maciasvictoreduardo5736@gmail.com>
2026-01-30 19:24:38 -0600

    acabando clase 66 angular basico

--- angular-basico-store/store/src/app/domains/products/pages/list/list.ts
+++ angular-basico-store/store/src/app/domains/products/pages/list/list.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../components/product/product';
 import { ProductModel } from '@shared/componentes/counter/models/product.model';
@@ -6,11 +6,12 @@ import { Cart } from '@shared/services/cart';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category';
 import { Category } from '@shared/componentes/counter/models/category.model';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, Product, ],
+  imports: [CommonModule, Product, RouterLink],
   templateUrl: './list.html',
   styleUrl: './list.css'
 })
@@ -20,10 +21,14 @@ export class List {
   private cartService = inject(Cart)
   private categoryService = inject(CategoryService)
   private productService = inject(ProductService);
+  @Input() category_id?: string;
 
   ngOnInit() {
-    this.getProducts()
     this.getCategories()
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+      this.getProducts()
   }
 
   addToCart(product: ProductModel) {
@@ -31,7 +36,7 @@ export class List {
   }
 
   private getProducts() {
-    this.productService.getProducts()
+    this.productService.getProducts(this.category_id)
     .subscribe({
       next: (products) => {
         this.products.set(products);
