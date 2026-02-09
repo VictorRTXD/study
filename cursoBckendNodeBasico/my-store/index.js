809a1763f3a95e8be111bf64139dda284fc6e23e
Victor <89050826+VictorRTXD@users.noreply.github.com>
2026-02-09 17:36:27 +0000

    completando clase 3 curso backend node js

--- cursoBckendNodeBasico/my-store/index.js
+++ cursoBckendNodeBasico/my-store/index.js
@@ -1,2 +1,11 @@
-console.log("aweso");
-console.log(1+"1");
+const express = require("express")
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(port, () => {
+  console.log("corriendo en " + port);
+})
